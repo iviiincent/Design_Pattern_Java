27108d434bb5937aa1295e5afd12edf8adb2ef4e
Vincent M <keel_direst_0r@icloud.com>
2025-08-15 14:39:08 +0800

    Import exercise skeleton

---
+++ src/main/java/io/github/iviiincent/exercise_02/FileIO.java
@@ -0,0 +1,15 @@
+package io.github.iviiincent.exercise_02;
+
+import java.io.IOException;
+
+public interface FileIO {
+
+    void readFromFile(String filename) throws IOException;
+
+    void writeToFile(String filename) throws IOException;
+
+    void setValue(String key, String value);
+
+    String getValue(String key);
+
+}
---
+++ src/main/java/io/github/iviiincent/exercise_02/FileProperties.java
@@ -0,0 +1,26 @@
+package io.github.iviiincent.exercise_02;
+
+import java.io.IOException;
+
+public class FileProperties implements FileIO {
+
+    @Override
+    public void readFromFile(String filename) throws IOException {
+        
+    }
+
+    @Override
+    public void writeToFile(String filename) throws IOException {
+
+    }
+
+    @Override
+    public void setValue(String key, String value) {
+
+    }
+
+    @Override
+    public String getValue(String key) {
+        return "";
+    }
+}
