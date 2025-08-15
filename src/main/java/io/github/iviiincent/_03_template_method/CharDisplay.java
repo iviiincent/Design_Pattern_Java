4126fd9f35ed4c720e4a966bc7e6e1d99f82852e
Vincent M <keel_direst_0r@icloud.com>
2025-08-15 20:39:38 +0800

    Finish 03-template-method

---
+++ src/main/java/io/github/iviiincent/_03_template_method/CharDisplay.java
@@ -0,0 +1,26 @@
+package io.github.iviiincent._03_template_method;
+
+public class CharDisplay extends AbstractDisplay {
+
+    private char ch;
+
+    public CharDisplay(char ch) {
+        this.ch = ch;
+    }
+
+    @Override
+    public void open() {
+        System.out.print("<<");
+    }
+
+    @Override
+    public void print() {
+        System.out.print(ch);
+    }
+
+    @Override
+    public void close() {
+        System.out.println(">>");
+    }
+    
+}
