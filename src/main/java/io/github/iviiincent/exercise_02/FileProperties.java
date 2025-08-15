3d531843aa23790fd2e8c9a3b55cdd7bf6f570ff
Vincent M <keel_direst_0r@icloud.com>
2025-08-15 14:53:46 +0800

    Build adapter for java.util.Properties via delegation

--- src/main/java/io/github/iviiincent/exercise_02/FileProperties.java
+++ src/main/java/io/github/iviiincent/exercise_02/FileProperties.java
@@ -1,26 +1,31 @@
 package io.github.iviiincent.exercise_02;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.Properties;
 
 public class FileProperties implements FileIO {
+    
+    Properties properties = new Properties();
 
     @Override
     public void readFromFile(String filename) throws IOException {
-        
+        InputStream inputStream = new FileInputStream(filename);
+        properties.load(inputStream);
     }
 
     @Override
     public void writeToFile(String filename) throws IOException {
-
+        OutputStream outputStream = new FileOutputStream(filename);
+        properties.store(outputStream, "written by FileProperties");
     }
 
     @Override
     public void setValue(String key, String value) {
-
+        properties.setProperty(key, value);
     }
 
     @Override
     public String getValue(String key) {
-        return "";
+        return properties.getProperty(key, "");
     }
 }
