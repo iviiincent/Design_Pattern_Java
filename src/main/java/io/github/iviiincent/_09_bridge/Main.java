f91c3e0fcc1d41fb2a68fe15dafe518a62767ec7
Vincent M <keel_direst_0r@icloud.com>
2025-08-18 13:31:15 +0800

    Finish 09-bridge

---
+++ src/main/java/io/github/iviiincent/_09_bridge/Main.java
@@ -0,0 +1,15 @@
+package io.github.iviiincent._09_bridge;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Display d1 = new Display(new StringDisplayImpl("Hello, Japan."));
+        Display d2 = new CountDisplay(new StringDisplayImpl("Hello, Asia."));
+        CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello, World."));
+
+        d1.display();
+        d2.display();
+        d3.display();
+        d3.multiDisplay(3);
+    }
+}
