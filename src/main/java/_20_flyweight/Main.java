7a9c91b4acd55bb17056fffeabd77074d449f8ff
Vincent M <keel_direst_0r@icloud.com>
2025-08-22 18:05:54 +0800

    20-2 test memory usage

--- src/main/java/_20_flyweight/Main.java
+++ src/main/java/_20_flyweight/Main.java
@@ -2,9 +2,23 @@ package _20_flyweight;
 
 public class Main {
 
+    private static final BigString[] bigStrings = new BigString[10000];
+
     public static void main(String[] args) {
-        new BigString("123123", true).print();
-        new BigString("123123", false).print();
+        testAllocation(false);
+        testAllocation(true);
+    }
+
+    public static void testAllocation(boolean shared) {
+        for (int i = 0; i < bigStrings.length; i++) {
+            bigStrings[i] = new BigString("123123", shared);
+        }
+        showMemory();
+    }
+
+    private static void showMemory() {
+        Runtime.getRuntime().gc();
+        System.out.println(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
     }
 
 }
